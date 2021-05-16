8a97a08a431675ae7e0b126b4bb93908ec34344f
melahat zıkkelleci <78692322+melahatz@users.noreply.github.com>
2021-05-16 22:50:08 +0300

    Add files via upload

---
+++ küme.py
@@ -0,0 +1,15 @@
+#liste,sözlük ve demet veri türü gibi birden çok veri türünü birlikte barındıran veri tipidir. Kümeler(set) ile ilgili yaptığınız her türlü
+#işlevi(birleşme,kesişim vs.) bu veri tipi ile de yapabilrisiniz.
+
+m={ "python",1,2,3,4,5,"learning","python","Icoding","python","python","java"}
+print(m) #çıktısında pythondan bir tane yazdırmış olucak.
+
+a=set("strong")
+print(a) #çıktısı küme şeklinde -> s,t,r,o,n,g
+len(m)
+print(len(m)) #python yine 1 olarak sayılıyor çıktısı 9 olur
+print("\n")
+
+#.add eklemek için kullanıyorduk
+m.add("html") 
+print(m)
