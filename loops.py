558356a94e4d5b400353cd32953c9cc7ee5efae2
melahat zıkkelleci <78692322+melahatz@users.noreply.github.com>
2021-05-16 22:51:29 +0300

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,10 @@
+#1döngüler birbirine benzeyen şeyleri tekrarlatmak için kullanılır.Listeleri dolaşırken kullanabiliriz.
+
+sehirler=["Ankara","İstanbul","Kayseri"]
+
+for sehir in sehirler :  #sehirlerin içinde dolaş ve o an şehire takma isim veriyoruz sehir gibi.
+    print(sehir)
+
+for sayi in range(1,10,2): #1 den 10 a kadar tek sayıları yazdır sondaki artış miktarı
+    print(sayi)
+
